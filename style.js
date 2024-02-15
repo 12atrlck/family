c206e90abc6c3ce18b72e263c19db239376cf450
12atrlck <158470923+12atrlck@users.noreply.github.com>
2024-02-15 14:40:13 +0100

    Patrick

---
+++ style.js
@@ -0,0 +1,14 @@
+alert ("hello");
+typeof (50);
+
+typeof ("hello");
+
+var myName="patrick";
+var nice="alright"
+
+
+alert("hello"+" "+"world");
+alert (myName+" there, "+nice)
+
+var tweet= promt("compose your message")
+var tweetCount= tweet.Length
